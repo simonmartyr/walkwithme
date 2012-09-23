b7b0a32ac84f86c79400b83082e9acf8a77200b2
Simon Martyr <simartyr@googlemail.com>
2012-09-23 16:33:23 +0100

    Tidy up JS
    Long overdue.

--- Scripts/locate.js
+++ Scripts/locate.js
@@ -6,8 +6,6 @@
 	coords.latitude
 	coords.longitude
 	.timestamp
- 
- 
  */
  
  /**********Global *******************/
@@ -30,85 +28,79 @@ $(document).ready(function()
 
 function createNew() //Clear array start afresh
 { 
-	routeJSON = [];
-	startLocation = "";
-	settupLocator();
-	snapLocation();
-	mapSettup();
-
+  routeJSON = [];
+  startLocation = "";
+  settupLocator();
+  snapLocation();
+  mapSettup();
 }
 	
-function mapSettup()
+function mapSettup() //settup Gmaps within the correct div - start co-ods
 {
-
-	if(startLocation == "") {
-        setTimeout(mapSettup, 50);//wait 50 millisecnds then recheck
-        return;
-    }
-		
-	map = new GMaps({
-		div: '#map',
-		lat: startLocation[0],
-		lng: startLocation[1],
-	});
-	
-	map.addMarker({
-			lat: startLocation[0],
-			lng: startLocation[1],
-			});
-	
+  if(startLocation == "") {
+    setTimeout(mapSettup, 50); //wait 50 millisecnds then recheck
+    return;
+  }
+  map = new GMaps({
+    div: '#map',
+    lat: startLocation[0],
+    lng: startLocation[1],
+    });
+  map.addMarker({
+    lat: startLocation[0],
+    lng: startLocation[1]
+    });
+  map.setZoom(17); // default 15 
 }
 
 
 function settupLocator () //check if geolocation enabled or use google. 
 {
-	try 
-	{
-		if (typeof navigator.geolocation === 'undefined')
-			locate = google.gears.factory.create('beta.geolocation'); 
-		else 
-			locate = navigator.geolocation;		
-	} 
-	catch(e) {}
-	if (!locate) 
-		alert("Geolocation services are not supported by your web browser.");	
+  try 
+  {
+    if (typeof navigator.geolocation === 'undefined')
+      locate = google.gears.factory.create('beta.geolocation'); 
+    else 
+      locate = navigator.geolocation;		
+  } 
+  catch(e) {}
+  if (!locate) 
+  alert("Geolocation services are not supported by your web browser.");	
 }
  
-function snapLocation () 
-{ //take the snapshot insert into array
-		locate.getCurrentPosition(savePosition, displayError);			
+function snapLocation () //take the snapshot insert into array
+{ 
+  locate.getCurrentPosition(savePosition, displayError);			
 }
-
 
 function savePosition(position)   //take data Put in local json
 { 
-	var locationToSave = 
-	{  
-			"time" 				: position.timestamp , 
-			"latitude"   : position.coords.latitude ,
-			"longitude": position.coords.longitude 
-	};
-			
-	if(startLocation  == "") //first time settup
-	{
-		startLocation = [position.coords.latitude, position.coords.longitude];
-		routeJSON.push(locationToSave);
-	}
-	else
-	{
-		routeJSON.push(locationToSave);
-		drawRoute();
-	}
+  var locationToSave = 
+  {  
+    "time" : position.timestamp , 
+    "latitude" : position.coords.latitude ,
+    "longitude" : position.coords.longitude 
+  };		
+  if(startLocation  == "") //first time settup
+  {
+    startLocation = [position.coords.latitude, position.coords.longitude];
+    routeJSON.push(locationToSave);
+  }
+  else
+  {
+    routeJSON.push(locationToSave);
+    drawRoute();
+  }
 }
 
 function drawRoute() // as we snapshot we draw
-{  
-	var lastIndex = routeJSON.length - 1
-		map.drawRoute //redraw
-		({ 
-			origin: [routeJSON[lastIndex - 1]["latitude"], routeJSON[lastIndex - 1]["longitude"]], 
-			destination: [routeJSON[lastIndex]["latitude"], routeJSON[lastIndex]["longitude"]]
-		});
+{
+  var lastIndex = routeJSON.length - 1;
+  map.drawRoute //redraw
+  ({ 
+    origin: [routeJSON[lastIndex - 1]["latitude"], routeJSON[lastIndex - 1]["longitude"]], 
+	destination: [routeJSON[lastIndex]["latitude"], routeJSON[lastIndex]["longitude"]]
+	});
 }
 
 function saveRoute() // the JSON array will be commited to the database here
