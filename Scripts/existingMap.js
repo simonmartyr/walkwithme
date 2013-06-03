d012b013b5944d8f3a5d65a8b1685f1e17e7b761
Simon Martyr <simartyr@googlemail.com>
2013-06-03 19:45:07 +0100

    gmap fix plus playback changes
    gmaps updated all good. and playback has direction added.
    
    animation to be added.

--- Scripts/existingMap.js
+++ Scripts/existingMap.js
@@ -4,6 +4,7 @@ var ExistingMap = (function(){
   var mapOptions;
   var startLocation;
   var options =  true;
+ 
 	
   function ExistingMap (start)
 	{
@@ -18,21 +19,41 @@ var ExistingMap = (function(){
 	
 	
 	ExistingMap.prototype.drawRoute = function (route){
+	  var countSymbols = 0 ; //marker each 5% of the route. 
+	  var fivePer = Math.round(route.length / Math.round(route.length * 0.02) );
 	  for(var i = 0; i < route.length; i++){
 		if(i + 1 != route.length){
 	      var toDraw = [[route[i]["latitude"], route[i]["longitude"]],  [route[i +1]["latitude"], route[i+1]["longitude"]]];
 		  draw(toDraw);
 		 }
+		 countSymbols++;
 	  }
 	  function draw(path){
+	    if(countSymbols %  fivePer == 0){
+			var lineSymbol = {
+				path : google.maps.SymbolPath.FORWARD_CLOSED_ARROW,
+				strokeColor: '#B22222',
+				 scale: 2,
+				strokeOpacity:  1
+			};
+		}
+		else
+		{
+		  var lineSymbol = ""; 
+		}
 		 mapOptions.drawPolyline 
 		({ 
 			path: path ,
-			strokeColor: '#131540',
-			strokeOpacity: 0.6,
-			strokeWeight: 4
+			strokeColor: '#202BAD',
+			strokeOpacity: 0.4, 
+			icons: [{ 
+				icon: lineSymbol,
+				offset: '50%'
+				}],
+			click: function(e) { alert("click");}
 		});
 	  }
+	  
 	};
 	
 	 ExistingMap.prototype.sizeChange =  function() //change of resoultion fill up screen area. 
